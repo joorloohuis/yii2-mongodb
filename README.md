3608ce34dafbb3dd6a1aafb2a4b00675233556e0
Joor Loohuis <joor.loohuis@fonq.nl>
2019-02-28 12:12:03 +0100

    inital codebase

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # yii2-mongodb
-Yii2 component that provides an interface to the official mongodb.org client
+
+MongoClient exposes official MongoDB support as provided by mongodb.org in the form of a Yii2 component. This component provides a MongoDB\Client object, and as such may be used exactly as documented in
+[the official MongoDB referece](https://docs.mongodb.com/php-library/).
+
+The reason behind the existence of this component is that the standard Yii2 component caters to the AR approach, and this shields a lot of the powerful core functionality provided by the MongoDB PHP Library provided by mongodb.org.
+
+This component requires the newer mongodb PHP extension, the old mongo PHP extension is untested and unsupported.
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```bash
+php composer.phar require joorloohuis/yii2-mongodb
+```
+
+or add
+
+```json
+"joorloohuis/yii2-mongodb": "^1.0.0"
+```
+
+to the require section of your `composer.json` file.
+
+## Configuration
+
+Add the following section to the `components` part of your configuration:
+
+```php
+<?php
+    'mongoclient' => [
+        'class' => \joorloohuis\mongodb\components\MongoClient::class,
+        'dsn' => 'mongodb://mongouser:mongopw@localhost:27017/database?args',
+        // alternatively, provide separate parameters
+        // 'host' => 'localhost',
+        // 'port' => 27017,
+        // 'user' => 'mongouser',
+        // 'password' => 'mongopw',
+        // 'db' => 'mongodatabase',
+        // 'args' => 'args',
+    ];
+```
+
+## Usage
+
+The MongoClient can be used for any operations the MongoDB PHP Library supports, for example:
+
+```php
+$collection = \Yii::$app->mongoclient->database->selectCollection('mycollection');
+$cursor = $collection->aggregate([
+    // aggregation pipeline parts go here
+]);
+$result = $cursor->toArray();
+```
---
+++ src/components/MongoClient.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace joorloohuis\mongodb\components;
+
+use yii\base\BaseObject;
+use yii\base\InvalidConfigException;
+use MongoDB\Client;
+use MongoDB\Driver\Exception\ConnectionTimeoutException;
+
+/**
+ * MongoClient
+ *
+ * Exposes official MongoDB support as provided by mongodb.org.
+ * This component provides a MongoDB\Client object, and as such
+ * may be used exactly as documented in
+ * https://docs.mongodb.com/php-library/v1.3/reference/
+ *
+ * The reason behind the existence of this class is that the standard
+ * Yii2 component caters to the AR approach, and this shields a lot
+ * of the powerful core functionality provided by the MongoDB PHP Library
+ * provided by mongodb.org.
+ *
+ * This component requires the newer mongodb PHP extension, the old mongo
+ * PHP extension is untested and unsupported.
+ *
+ * TODO:
+ * * add more configuration parameters
+ * * add support for alternative authentication database support
+ */
+class MongoClient extends BaseObject
+{
+    /**
+     * DSN for connecting with MongoDB
+     *
+     * @var string
+     */
+    public $dsn;
+
+    /**
+     * host
+     *
+     * @var string
+     */
+    public $host = 'localhost';
+
+    /**
+     * port
+     *
+     * @var integer
+     */
+    public $port = 27017;
+
+    /**
+     * database name
+     *
+     * @var string
+     */
+    public $db;
+
+    /**
+     * database user
+     *
+     * @var string
+     */
+    public $user;
+
+    /**
+     * database password
+     *
+     * @var string
+     */
+    public $password;
+
+    /**
+     * connection arguments
+     *
+     * @var string
+     */
+    public $args = '';
+
+    /**
+     * Connected database
+     *
+     * @var MongoDB\Database
+     */
+    public $database;
+
+    /**
+     * MongoDB client
+     *
+     * @var MongoDB\Client
+     */
+    protected $client;
+
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+    }
+
+    public function init()
+    {
+        parent::init();
+        $this->connect();
+    }
+
+    protected function connect()
+    {
+        if ($this->dsn) {
+            $this->db = preg_replace('/(.*\/|\?.*)/', '', $this->dsn);
+        } else {
+            $this->dsn = sprintf("mongodb://%s:%s@%s:%s/%s",
+                $this->user,
+                $this->password,
+                $this->host,
+                $this->port,
+                $this->db
+            );
+            if ($this->args) {
+                $this->dsn .= '?' . $args;
+            }
+        }
+        $this->client = new Client($this->dsn);
+        $this->database = $this->client->selectDatabase($this->db);
+    }
+
+    // funnel any call to an unknown method to the MongoDB client if that provides it
+    public function __call($method, $args)
+    {
+        if (method_exists($this->client, $method)) {
+            return call_user_func_array([$this->$client, $method], $args);
+        }
+    }
+
+}
